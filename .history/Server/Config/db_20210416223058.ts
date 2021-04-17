756b5508340de4e6ffcfabb7902725e6e1d37d12
jaisydh07 <jasmailkaur.dhaliwal@dcmail.ca>
2021-04-16 22:34:24 -0400

    final commit

--- .history/Server/Config/db_20210416223058.ts
+++ .history/Server/Config/db_20210416223058.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/webd6201";
-export const RemoteURI = "mongodb+srv://jaisydh07:Dhaliwal99@webd6201.4ryhk.mongodb.net/webd6201?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://jaisydh07:Laddi0599@webd6201.4ryhk.mongodb.net/webd6201?retryWrites=true&w=majority";
 export const Secret = "someSecret";
 export const Host = "MongoDB Atlas";
